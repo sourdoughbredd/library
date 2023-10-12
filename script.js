eba5ca19607dd4ca6e2664b84770f25589f8a47a
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-10-12 16:19:16 -0400

    Display all books in my library on page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const cardContainer = document.querySelector(".card-container");
+
 const myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -11,11 +13,29 @@ function Book(title, author, pages, isRead) {
     };
 }
 
-function addBookToLibrary(title, author, page, isRead) {
-    myLibrary.push(new Book(title, author, page, isRead));
+function addBookToLibrary(title, author, pages, isRead) {
+    myLibrary.push(new Book(title, author, pages, isRead));
+}
+
+function addBookToDisplay(book){
+    let card = document.createElement("div");
+    card.classList.add('card');
+    card.innerHTML = `
+        <h2>${book.title}</h2>
+        <h3>Author: ${book.author}</h3>
+        <p>Pages: ${book.pages}</p>
+        <p>Read by you: ${book.isRead ? "Yes" : "No"}
+    `;
+    cardContainer.appendChild(card);
+}
+
+function displayAllBooks(library) {
+    myLibrary.forEach(book => addBookToDisplay(book));
 }
 
 addBookToLibrary('good book', 'good author', 10, true);
 addBookToLibrary('okay book', 'okay author', 100, true);
 addBookToLibrary('bad book', 'bad author', 1000, false);
 addBookToLibrary('awful book', 'awful author', 10000, false);
+
+displayAllBooks(myLibrary);
