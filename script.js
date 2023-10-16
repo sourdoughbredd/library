e89725bd013581d222bbb6486f7cd596a58eadd4
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-10-16 14:25:25 -0400

    Add remove/toggle-read buttons to each book

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const cardContainer = document.querySelector(".card-container");
 
 const myLibrary = [];
+const toggleReadButtons = [];
+const removeBookButtons = [];
 
 function Book(title, author, pages, isRead) {
     this.title = title;
@@ -17,10 +19,11 @@ function addBookToLibrary(title, author, pages, isRead) {
     myLibrary.push(new Book(title, author, pages, isRead));
 }
 
-function addBookToDisplay(book){
+function addBookToDisplay(book, index){
     let card = document.createElement("div");
     card.classList.add('card');
     card.classList.add('book');
+    card.setAttribute('data-index', index.toString())
     if (book.isRead) {
         card.classList.add('is-read');
     }
@@ -29,12 +32,16 @@ function addBookToDisplay(book){
         <h3 class="author">by ${book.author}</h3>
         <p class="pages">Pages: ${book.pages}</p>
         <div class="read-container"></div>
+        <div class="actions-container"> 
+            <button class="remove-book">Remove from Library</button>
+            <button class="toggle-read">Mark as Read</button>
+        </div>
     `;
     cardContainer.appendChild(card);
 }
 
 function displayAllBooks(library) {
-    myLibrary.forEach(book => addBookToDisplay(book));
+    myLibrary.forEach((book, index) => addBookToDisplay(book, index));
 }
 
 addBookToLibrary('good book', 'good author', 10, true);
@@ -43,3 +50,5 @@ addBookToLibrary('bad book', 'bad author', 1000, false);
 addBookToLibrary('awful book', 'awful author', 10000, false);
 
 displayAllBooks(myLibrary);
+
+
